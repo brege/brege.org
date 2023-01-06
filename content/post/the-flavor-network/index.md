4f4997f75b53d26db2b59d8d0df7d7fc365cbb5e
Wyatt Brege <wyatt@brege.org>
2023-01-06 07:51:33 -0500

    Add source files for flavor network

---
+++ content/post/the-flavor-network/index.md
@@ -0,0 +1,43 @@
+---
+title: 'The Flavor Network'
+authors: 'Wyatt Brege'
+tags: ['flavor', 'network', 'visjs', 'graph', 'jaccard', 'metric']
+date: 2023-01-04T04:04:49-05:00
+cover:
+  image: '' 
+  caption: '' 
+jquery: true
+draft: false
+---
+
+{{< flavor-network 
+  nodesPath="/data/flavor/nodes.json" 
+  edgesPath="/data/flavor/edges.json"
+  simPath="/data/flavor/similarity.json" >}}
+
+{{< search-plots jsonPath="/data/flavor/nodes.json" >}}
+
+This tool allows you to explore the flavor network, a social graph for flavor profiles.  The network is based on the [Flavor Bible](https://karenandandrew.com/books/the-flavor-bible/) and soon the companion book [What to Drink with What You Eat](https://karenandandrew.com/books/what-to-drink-with-what-you-eat/).
+
+You search for an ingredient you like, and the graph refines based on a similarity score between ingredients (in the flavor metric).  Click on an ingredient in the network to add it to your recipe above the search box, or to remove it.
+
+Understanding why I chose this book for the dataset are mostly apparent, but the key thing is the authors did a fine job formatting something computer readable and human usable--a rare feat!  Most importantly, it is aggregated from *chefs*, from real humans in kitchens doing what works, what's delicious, and what's in season.  Recipe API's don't have this granularity, they rely too heavily on user data to seed recommendatinos.  To my knowledge, this is the only dataset of this kind.
+
+Technical tools only involve [vis.js](https://visjs.org/) for visualization and [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) for parsing.  The data is scraped from the Flavor Bible, and the similarity matrix is calculated using [Jaccard similarity](https://en.wikipedia.org/wiki/Jaccard_index) for [pairwaise comparisons](https://en.wikipedia.org/wiki/Pairwise_comparison). The [Natural Language Toolkit](https://www.nltk.org/) is used to clean up ingredient names.  
+
+In 2019, I was helping fellow chefs come up with new menu offerings for our farm-to-fork specials, where a strategy eventually developed to prompt ourselves with constraints:
+1. do something new
+2. use something old
+3. feature three things seasonal
+
+We would scrap together new ideas over the prep table.  As ideas began to stretch and the Northern California winter approached, we sometimes used sheets of butcher paper to draw out a crude graph of our plate setups.  Sketches of sauce and protein layouts, heavy edges between ideas if the pairing 'sang', hanging in the ticket window on feature nights for guidance. Karen and Andrew's book was gifted to me later that year, and it changed my game.  I had a good resource, one *with a chef's trust*.
+
+This method was so helpful, I started dreaming of a computer tool to do the same. I saw a reddit [post](https://www.reddit.com/r/datasets/comments/3bxlg7/i_have_every_publicly_available_reddit_comment/) that layed out some of these ideas: overlapping subreddits :: compatable flavors.  Sadly, restaurant life is not at all conducive to finding free time to write code.
+
+I believe the impact of mathematical concepts to culinary scope to be a major upgrade in food-first thoughtfulness.  To extend its application to creativity and clarity, not abused for statistics and business evils. While I do see how these kinds of tools do have immense practical application in the distribution world, and with grander planning, the drive here is to empower chefs, bartenders, brewers, baristas, and sommeliers.
+
+When you have writer's block, having suggestions from a trustful source is a big advantage.
+
+Chef's unfortuanely are at a disadvantage creatively when compared to some other impactful disciplines (music/language).  Yes, we have recipes, but those are instructions, and never help us build on [ratio](https://ruhlman.com/ruhlmans-books/) or [balance](https://www.saltfatacidheat.com/).  When you're on the couch in your jammies all you've really got is inspirations from social media and interactions with your peers.  
+
+I'm hoping this tool can help with creativity the same way musicians and writers have sofa friendly rubrics to make new.  Where's the guitar tabs and spell checker for food?
---
+++ static/js/flavor-network.js
@@ -0,0 +1,243 @@
+// Define the color palettes
+// https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+
+// this is a poor way to do this, but it works for now
+const greyColorPalette = [
+  '#fafafa',  // Grey 100
+  '#f5f5f5',  // Grey 200
+  '#eeeeee',  // Grey 300
+  '#e0e0e0',  // Grey 400
+  '#bdbdbd',  // Grey 500
+  '#9e9e9e',  // Grey 600
+  '#757575',  // Grey 700
+  '#616161',  // Grey 800
+  '#424242',  // Grey 900
+  '#212121',  // Grey 1000
+];
+const blueColorPalette = [
+  '#E3F2FD',  // Light Blue 100
+  '#BBDEFB',  // Light Blue 200
+  '#90CAF9',  // Light Blue 300
+  '#64B5F6',  // Light Blue 400
+  '#42A5F5',  // Light Blue 500
+  '#2196F3',  // Light Blue 600
+  '#1E88E5',  // Light Blue 700
+  '#1976D2',  // Light Blue 800
+  '#1565C0',  // Light Blue 900
+  '#0D47A1',  // Light Blue 1000
+];
+const greenColorPalette = [
+  '#E8F5E9',  // Light Green 100
+  '#C8E6C9',  // Light Green 200
+  '#A5D6A7',  // Light Green 300
+  '#81C784',  // Light Green 400
+  '#66BB6A',  // Light Green 500
+  '#4CAF50',  // Light Green 600
+  '#43A047',  // Light Green 700
+  '#388E3C',  // Light Green 800
+  '#2E7D32',  // Light Green 900
+  '#1B5E20',  // Light Green 1000
+];
+
+// Define a function for the nodes based on a color palette
+function generateNodeStyles(colorPalette) {
+  return {
+    nodes: {
+      color: {
+        background: colorPalette[3],  // Pallete 400
+        border: colorPalette[5],  // Pallete 600  
+        highlight: {
+          background: colorPalette[3],  // Pallete 700
+          border: colorPalette[5],  // Pallete 600
+        },
+        hover: {
+          background: colorPalette[5],  // Pallete 600
+          border: colorPalette[7],  // Pallete 600
+        },
+      },
+      font: {
+        color: colorPalette[9],  // Pallete 1000
+        size: 24,
+        align: 'center',
+
+      },
+      shape: 'box',
+      shapeProperties: {
+        borderRadius: 8,
+      },
+    },
+    edges: {
+      color: {
+        color: colorPalette[3],  // Pallete 600
+        highlight: colorPalette[3],  // Pallete 600
+        hover: colorPalette[3],  // Pallete 600
+      },
+      width: 1,
+    },
+
+  };
+};
+
+const nodeStylesOrigin = generateNodeStyles(blueColorPalette);
+const nodeStylesPrime = generateNodeStyles(greyColorPalette);
+
+// Define the network options
+const options = {
+  interaction: {
+    hover: true,
+    hoverConnectedEdges: true,
+  },
+  autoResize: true,
+  height: '100%',
+  width: '100%',
+};
+
+/* Goals:
+  * 1. The users specifies a node or a list of nodes (selectedResults)
+  * 2. The nodes are displayed on the network graph
+  * 3. From those nodes, we find the nodes that are most similar to them
+  * 4. The most similar nodes are displayed on the network graph
+  * 
+  * We do not display nodes that are not similar to the selected nodes
+  * or nodes that are not selected
+  * 
+  * Just for reference, similarities looks like this:
+  *  { node1: { node2: 0.5, node3: 0.3, ... }, node2: { node1: 0.5, node3: 0.2, ... }, ... }
+  * 
+  * The nodes and edges are displayed on the network graph
+  */
+
+// get the nodes and edges from the server
+
+// Create a new network graph
+const container = document.getElementById('network');
+const network = new vis.Network(container, {}, options);
+const nodesPath = $("#network").data("nodes-path");
+const edgesPath = $("#network").data("edges-path");
+const simPath = $("#network").data("sim-path");
+
+let nodes = [];
+let edges = [];
+let similarities = {};
+
+// get the nodes and edges from the server
+async function getNodesAndEdges() {
+  const nodes = await $.getJSON(nodesPath);
+  const edges = await $.getJSON(edgesPath);
+  return { nodes, edges };
+}
+// get the similarities from the server
+async function getSimilarities() {
+  const similarities = await $.getJSON(simPath);
+  //console.log('similarities', similarities);
+  return similarities;
+}
+
+// get the nodes and edges from the server
+getNodesAndEdges().then(function (data) {
+  nodes = data.nodes;
+  edges = data.edges;
+  //console.log('nodes', nodes);
+  //console.log('edges', edges);
+  // get the similarities from the server
+  getSimilarities().then(function (data) {
+    similarities = data;
+  });
+});
+
+document.addEventListener('selectedResultsChanged', function (e) {
+  // get the selected results
+  const selectedResults = e.detail;
+  //console.log('selectedResults', selectedResults);
+  // Filter the nodes and edges based on the selected results
+  filterNodesAndEdges(selectedResults);
+});
+
+// When the user clicks a node
+network.on('click', function (params) {
+  if (params.nodes.length > 0) {
+    const nodeId = params.nodes[0];
+    handleResultClick(nodeId);
+  }
+});   
+
+// Filter the nodes and edges based on the selected results
+function filterNodesAndEdges(selectedResults) {
+  console.log('selectedResults', selectedResults);
+
+  /** functionality **/
+
+  // get the nodes that are similar to the selected nodes
+  const similarNodes = getSimilarNodes(selectedResults);
+  console.log('similarNodes', similarNodes);
+
+  // filter the nodes and edges based on the selected results
+  let filteredNodes = nodes.filter(function (node) {
+    return similarNodes.map(function (item) {
+      return item[0];
+    }).includes(node.id) || selectedResults.includes(node.id);
+  });
+  // 
+  let filteredEdges = edges.filter(function (edge) {
+    return similarNodes.map(function (item) {
+      return item[0];
+    }).includes(edge.from) && similarNodes.map(function (item) {
+      return item[0];
+    }).includes(edge.to);
+  });
+  //console.log('filteredNodes', filteredNodes);
+  //console.log('filteredEdges', filteredEdges);
+  
+  // set the nodes and edges on the network graph
+  network.setOptions(nodeStylesPrime);
+  network.setData({ nodes: filteredNodes, edges: filteredEdges });
+
+  /** cosmetic **/
+
+  // change the color of the nodes that are selectedResults
+  selectedResults.forEach(function (nodeId) {
+    network.body.data.nodes.update({ 
+      id: nodeId, 
+      color: nodeStylesOrigin.nodes.color, 
+    });
+  });
+  // for any edge that is connected to a selectedResult only,
+  // change the color of the edge to the origin color
+  filteredEdges.forEach(function (edge) {
+    if (selectedResults.includes(edge.from) && !selectedResults.includes(edge.to)) {
+      network.body.data.edges.update({ 
+        id: edge.id, 
+        color: nodeStylesOrigin.edges.color, 
+        width: 3*nodeStylesOrigin.edges.width,
+      });
+    }
+    if (!selectedResults.includes(edge.from) && selectedResults.includes(edge.to)) {
+      network.body.data.edges.update({ 
+        id: edge.id, 
+        color: nodeStylesOrigin.edges.color, 
+        width: 3*nodeStylesOrigin.edges.width 
+      });
+    }
+  });
+
+  /* ************************************ */
+  /* I COULD CRY I FINALLY GOT IT TO WORK */
+  /* ************************************ */
+}
+
+// get the nodes that are similar to the selected nodes
+function getSimilarNodes(selectedNodes, n=7) {
+  const similarNodes = selectedNodes.map(function (node) {
+    const nodeSimilarities = similarities[node];
+    const sortedSimilarities = Object.entries(nodeSimilarities).sort(function (a, b) {
+      return b[1] - a[1];
+    });
+    const topNSimilarNodes = sortedSimilarities.slice(0, n);
+    return topNSimilarNodes;
+  });
+  const flattenedSimilarNodes = similarNodes.flat();
+  const uniqueSimilarNodes = [...new Set(flattenedSimilarNodes)];
+  //console.log('uniqueSimilarNodes', uniqueSimilarNodes);
+  return uniqueSimilarNodes;
+}
+
